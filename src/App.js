ff18ccff81049ef5650aef92f7eac0428c5ccac4
Najihah0125 <najihah0125@gmail.com>
2023-12-20 16:31:45 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,63 @@ function App() {
       <Footer />
       <Routes>
         {/* route of the web page */}
-        <Route exact path="/" element={<DisplayPurchase/>} />
+        {/* <Route exact path="/" element={<DisplayPurchase/>} /> */}
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-chartist" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-chartjs" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-lock-screen" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-login" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-recoverpw" element={<DisplayPurchase/>} />
+        <Route exact path="/auth-register" element={<DisplayPurchase/>} />
+        <Route exact path="/calendar" element={<DisplayPurchase/>} />
+        <Route exact path="/charts-apex" element={<DisplayPurchase/>} />
+        <Route exact path="/chat" element={<DisplayPurchase/>} />
       </Routes>
     </Router>
   );
