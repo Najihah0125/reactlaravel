98187b1e4ba1ee1e1eefe911b763bce1f4813715
Najihah0125 <najihah0125@gmail.com>
2023-12-29 17:53:20 +0800

    Named app.js -> utils.js, separate each function into multiple useEffect, but haven't test it out

--- src/Components/draft.jsx
+++ src/Components/draft.jsx
@@ -1,4 +1,4 @@
-<div
+{/* <div
 id="apexchartsed37aiax"
 class="apexcharts-canvas apexchartsed37aiax apexcharts-theme-light"
 style={{ width: "511px", height: "380px" }}
@@ -2646,3 +2646,71 @@ style={{ width: "511px", height: "380px" }}
 
 // export default ApexChart;
 
+
+
+
+
+
+//
+{/* <div id="world-map-markers" style="width: 600px; height: 400px;"></div>
+      <script>
+        $(function(){
+          $("#world-map-markers").vectorMap({
+            map: "world_mill_en",
+            normalizeFunction: "polynomial",
+            hoverOpacity: 0.7,
+            hoverColor: !1,
+            regionStyle: { initial: { fill: "#dde3ec" } },
+            markerStyle: {
+              initial: {
+                r: 9,
+                fill: "#525CE9",
+                "fill-opacity": 0.9,
+                stroke: "#fff",
+                "stroke-width": 7,
+                "stroke-opacity": 0.4,
+              },
+              hover: { stroke: "#fff", "fill-opacity": 1, "stroke-width": 1.5 },
+            },
+            backgroundColor: "transparent",
+            markers: [
+              { latLng: [41.9, 12.45], name: "USA" },
+              { latLng: [12.05, -61.75], name: "Russia" },
+              { latLng: [1.3, 103.8], name: "Australia" },
+            ],
+          });
+        });        
+      </script> */}
+
+
+//extra script
+// <!-- <script src="assets/js/app.js"></script> -->
+// <!-- <script src="Assets/admin-resources/jquery.vectormap/jquery-jvectormap-2.0.5.min.js" type="text/jsx"></script>
+//     <script src="Assets/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js" type="text/jsx"></script> -->
+
+
+// <!-- JAVASCRIPT -->
+//     <!-- <script src="assets/libs/jquery/jquery.min.js"></script>
+//     <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+//     <script src="assets/libs/simplebar/simplebar.min.js"></script>
+//     <script src="assets/libs/node-waves/waves.min.js"></script> -->
+
+//     <!-- apexcharts -->
+//     <!-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> -->
+
+//     <!-- Calendar init -->
+//     <!-- <script src="assets/js/pages/calendar.init.js"></script> -->
+//     <!-- plugin js -->
+//     <!-- <script src="assets/libs/moment/min/moment.min.js"></script>
+//     <script src="assets/libs/jquery-ui-dist/jquery-ui.min.js"></script>
+//     <script src="assets/libs/@fullcalendar/core/main.min.js"></script>
+//     <script src="assets/libs/@fullcalendar/bootstrap/main.min.js"></script>
+//     <script src="assets/libs/@fullcalendar/daygrid/main.min.js"></script>
+//     <script src="assets/libs/@fullcalendar/timegrid/main.min.js"></script>
+//     <script src="assets/libs/@fullcalendar/interaction/main.min.js"></script> -->
+
+
+//     <!-- Plugins js-->
+    
+//     <!-- <script src="assets/js/pages/dashboard.init.js"></script>
+//      --> */}
